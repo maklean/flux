5a48c4bf75093bb96dbda9a7eceb63ee0ad562ee
maklean <yoelmakaya@gmail.com>
2026-02-18 18:50:08 -0500

    impl: generate go code and use gin to start HTTP server

---
+++ server/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello, World!",
+		})
+	})
+
+	if err := r.Run(); err != nil {
+		log.Fatalf("failed to run server: %v", err)
+	}
+}
